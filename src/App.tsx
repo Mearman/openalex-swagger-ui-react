7f611452e78ff439b89b41225f34acab18869271
Joseph Mearman <joseph@mearman.co.uk>
2024-01-15 12:42:19 +0000

    Initial content

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+export default () => <SwaggerUI url="https://raw.githubusercontent.com/Mearman/openalex-api-spec/main/generated/openapi.json" />;
+// export default () => <SwaggerUI url="./openapi.json" />;
